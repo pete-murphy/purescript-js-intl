c25835a0c4f0a10df7e2d6c880c76032047fe8dc
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2023-06-07 14:14:37 -0400

    Refactor tests to use assertEqual

--- test/Main.purs
+++ test/Main.purs
@@ -12,14 +12,30 @@ import Web.Intl.DateTimeFormat as DateTimeFormat
 
 main :: Effect Unit
 main = do
+  test_DateTimeFormat
 
+test_DateTimeFormat :: Effect Unit
+test_DateTimeFormat = do
   Console.log "DateTimeFormat.supportedLocalesOf"
-  Test.assert (DateTimeFormat.supportedLocalesOf [ "en-US" ] { localeMatcher: "best fit" } == [ "en-US" ])
-  Test.assert (DateTimeFormat.supportedLocalesOf [ "en-US", "es-MX" ] { localeMatcher: "best fit" } == [ "en-US", "es-MX" ])
+  Test.assertEqual
+    { actual: DateTimeFormat.supportedLocalesOf [ "en-US" ] { localeMatcher: "best fit" }
+    , expected: [ "en-US" ]
+    }
+
+  Test.assertEqual
+    { actual: DateTimeFormat.supportedLocalesOf [ "en-US", "es-MX" ] { localeMatcher: "best fit" }
+    , expected: [ "en-US", "es-MX" ]
+    }
 
   Console.log "DateTimeFormat.supportedLocalesOf_"
-  Test.assert (DateTimeFormat.supportedLocalesOf_ [ "en-US" ] == [ "en-US" ])
-  Test.assert (DateTimeFormat.supportedLocalesOf_ [ "en-US", "es-MX" ] == [ "en-US", "es-MX" ])
+  Test.assertEqual
+    { actual: DateTimeFormat.supportedLocalesOf_ [ "en-US" ]
+    , expected: [ "en-US" ]
+    }
+  Test.assertEqual
+    { actual: DateTimeFormat.supportedLocalesOf_ [ "en-US", "es-MX" ]
+    , expected: [ "en-US", "es-MX" ]
+    }
 
   format <- DateTimeFormat.new_ [ "en-US" ]
   let
@@ -34,55 +50,74 @@ main = do
     date' = mkDate { month: 7.0, day: 23.0, year: 2013.0 }
 
   Console.log "DateTimeFormat##format"
-  Test.assert (DateTimeFormat.format format date == "12/21/2012")
+  Test.assertEqual
+    { actual: DateTimeFormat.format format date
+    , expected: "12/21/2012"
+    }
 
   Console.log "DateTimeFormat##formatRange"
-  Test.assert (DateTimeFormat.formatRange format date date == "12/21/2012")
-  Test.assert (DateTimeFormat.formatRange format date date' == "12/21/2012 – 8/23/2013")
+  Test.assertEqual
+    { actual: DateTimeFormat.formatRange format date date
+    , expected: "12/21/2012"
+    }
+  Test.assertEqual
+    { actual: DateTimeFormat.formatRange format date date'
+    , expected: "12/21/2012 – 8/23/2013"
+    }
 
   Console.log "DateTimeFormat##formatRangeToParts"
-  Test.assert do
-    DateTimeFormat.formatRangeToParts format date date ==
-      [ { type: "month", value: "12" }
-      , { type: "literal", value: "/" }
-      , { type: "day", value: "21" }
-      , { type: "literal", value: "/" }
-      , { type: "year", value: "2012" }
-      ]
-  Test.assert do
-    DateTimeFormat.formatRangeToParts format date date' ==
-      [ { type: "month", value: "12" }
-      , { type: "literal", value: "/" }
-      , { type: "day", value: "21" }
-      , { type: "literal", value: "/" }
-      , { type: "year", value: "2012" }
-      , { type: "literal", value: " – " }
-      , { type: "month", value: "8" }
-      , { type: "literal", value: "/" }
-      , { type: "day", value: "23" }
-      , { type: "literal", value: "/" }
-      , { type: "year", value: "2013" }
-      ]
+  Test.assertEqual
+    { actual: DateTimeFormat.formatRangeToParts format date date
+    , expected:
+        [ { type: "month", value: "12" }
+        , { type: "literal", value: "/" }
+        , { type: "day", value: "21" }
+        , { type: "literal", value: "/" }
+        , { type: "year", value: "2012" }
+        ]
+    }
+
+  Test.assertEqual
+    { actual: DateTimeFormat.formatRangeToParts format date date'
+    , expected:
+        [ { type: "month", value: "12" }
+        , { type: "literal", value: "/" }
+        , { type: "day", value: "21" }
+        , { type: "literal", value: "/" }
+        , { type: "year", value: "2012" }
+        , { type: "literal", value: " – " }
+        , { type: "month", value: "8" }
+        , { type: "literal", value: "/" }
+        , { type: "day", value: "23" }
+        , { type: "literal", value: "/" }
+        , { type: "year", value: "2013" }
+        ]
+    }
 
   Console.log "DateTimeFormat##formatToParts"
-  Test.assert do
-    DateTimeFormat.formatToParts format date ==
-      [ { type: "month", value: "12" }
-      , { type: "literal", value: "/" }
-      , { type: "day", value: "21" }
-      , { type: "literal", value: "/" }
-      , { type: "year", value: "2012" }
-      ]
+  Test.assertEqual
+    { actual: DateTimeFormat.formatToParts format date
+    , expected:
+        [ { type: "month", value: "12" }
+        , { type: "literal", value: "/" }
+        , { type: "day", value: "21" }
+        , { type: "literal", value: "/" }
+        , { type: "year", value: "2012" }
+        ]
+    }
 
   Console.log "DateTimeFormat##resolvedOptions"
   resolvedOptions <- DateTimeFormat.resolvedOptions format
-  Test.assert do
-    resolvedOptions ==
-      { locale: "en-US"
-      , calendar: "gregory"
-      , numberingSystem: "latn"
-      , timeZone: "America/New_York"
-      , year: "numeric"
-      , month: "numeric"
-      , day: "numeric"
-      }
+  Test.assertEqual
+    { actual: resolvedOptions
+    , expected:
+        { locale: "en-US"
+        , calendar: "gregory"
+        , numberingSystem: "latn"
+        , timeZone: "America/New_York"
+        , year: "numeric"
+        , month: "numeric"
+        , day: "numeric"
+        }
+    }
+
