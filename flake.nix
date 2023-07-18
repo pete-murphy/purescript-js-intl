e64ea32e4f7bfdd4be0b892e7f900f5a994c3f29
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2023-07-18 00:58:05 -0400

    Use new spago, purescript-overlay

--- flake.nix
+++ flake.nix
@@ -2,27 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    easy-purescript-nix = {
-      url = "github:justinwoo/easy-purescript-nix";
-      flake = false;
-    };
+    purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
+    purescript-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, easy-purescript-nix }:
+
+  outputs = { self, nixpkgs, flake-utils, purescript-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        easy-ps = import easy-purescript-nix { inherit pkgs; };
-      in
-      {
-        devShell = pkgs.mkShell {
+        overlays = [ purescript-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            easy-ps.purs
-            easy-ps.spago
-            easy-ps.purs-tidy
-            easy-ps.purescript-language-server
-            pkgs.nodejs
-            pkgs.dhall-lsp-server
-            pkgs.esbuild
+            pkgs.nodejs_18
+            pkgs.purs
+            pkgs.spago-unstable
+            pkgs.purs-tidy-bin.purs-tidy-0_10_0
+            pkgs.purs-backend-es
           ];
         };
       });
